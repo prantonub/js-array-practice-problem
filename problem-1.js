61259d21bcf3b2aa5dca6066bb00bc09001c7a55
prantonub <prantonub.cse23@gmail.com>
2025-06-04 21:08:15 +0600

    task added

--- problem-1.js
+++ problem-1.js
@@ -1,4 +1,3 @@
-// ### 1. Declare an array 
 // 1. Declare an array with 5 elements containing fruits
 // 2. console log the 3rd index element
 // 3. change the value of the 2nd index element to jambura
