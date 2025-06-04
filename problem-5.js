926a60da551c9ab379a7265d5950d3973729f103
prantonub <prantonub.cse23@gmail.com>
2025-06-04 21:07:33 +0600

    task added

--- problem-5.js
+++ problem-5.js
@@ -1,7 +1,5 @@
 // ### 5. Combining Arrays
 
-// **Instructions:**
-
 // 1. Create two arrays of your choice.
 // 2. Use the concat method to combine the two arrays into a new array.
 // 3. Print both the original arrays and the combined array using console.log().
