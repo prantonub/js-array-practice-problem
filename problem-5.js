c9c0797c08418844cb901270f37b3d2ec1797329
prantonub <prantonub.cse23@gmail.com>
2025-06-07 00:00:51 +0600

    task added

--- problem-5.js
+++ problem-5.js
@@ -1,9 +1,8 @@
 // ### 5. Combining Arrays
-
 // 1. Create two arrays of your choice.
 // 2. Use the concat method to combine the two arrays into a new array.
 // 3. Print both the original arrays and the combined array using console.log().
-//--------------------------------------------------------------------------------
+
 const fruits = ['Mango', 'Banana', 'Apple'];
 const vegetables = ['Carrot', 'Broccoli', 'Spinach'];
 
