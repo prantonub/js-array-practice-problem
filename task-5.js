258241042a2cdb0c76541a749da7f71dc03ab989
prantonub <prantonub.cse23@gmail.com>
2025-06-20 01:45:24 +0600

    task added

---
+++ task-5.js
@@ -0,0 +1,13 @@
+
+
+// 1. Create two arrays of numbers
+const oddNumbers = [1, 3, 5];
+const evenNumbers = [2, 4, 6];
+
+// 2. Combine the arrays
+const allNumbers = oddNumbers.concat(evenNumbers);
+
+// 3. Print all arrays
+console.log('Odd Numbers:', oddNumbers);
+console.log('Even Numbers:', evenNumbers);
+console.log('All Numbers:', allNumbers);
